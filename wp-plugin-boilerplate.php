2d279cc61a8ce3cfd796a358438a88527154c8a3
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-11-28 16:06:35 +0545

    Initial plugin initialization

---
+++ wp-plugin-boilerplate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WP Plugin Boilerplate
+ * Plugin URI: https://github.com/wpeverest/wp-plugin-boilerplate
+ * Description: Bootstrap for creating a WordPress plugin using Webpack and Git.
+ * Version: 1.0.0
+ * Author: WPEverest
+ * Author URI: https://wpeverest.com
+ * License: GPLv3 or later
+ * Text Domain: wp-plugin-boilerplate
+ * Domain Path: /languages/
+ *
+ * @package WP_Plugin_Boilerplate
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
